6f8c29707327baf39b6a271b2bb7ee73234a03f5
Ahmad Faris <isfawwaz@gmail.com>
2023-03-19 05:30:44 +0700

    feat: add from stash

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,7 @@
 module.exports = {
   root: true,
-  // This tells ESLint to load the config from the package `eslint-config-custom`
-  extends: ["custom"],
-  settings: {
-    next: {
-      rootDir: ["apps/*/"],
-    },
+  env: {
+    node: 'true',
   },
+  extends: ['@isfawwaz/eslint-config-base', '@isfawwaz/eslint-config-jest'],
 };
