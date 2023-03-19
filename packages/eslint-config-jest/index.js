0b4258d87cb947248ca32e7d92772b6a9c54fbb7
Ahmad Faris <isfawwaz@gmail.com>
2023-03-20 04:19:48 +0700

    fix: remove semver from eslint-config-jest

--- packages/eslint-config-jest/index.js
+++ packages/eslint-config-jest/index.js
@@ -1,5 +1,4 @@
 const testFilePatterns = require('@isfawwaz/eslint-config-base/lib/test-patterns');
-const semver = require('semver');
 
 module.exports = {
   overrides: [
@@ -8,8 +7,7 @@ module.exports = {
       plugins: ['jest'],
       extends: ['./rules/jest'].map(require.resolve),
       env: {
-        jest: semver.satisfies(process.version, '<14') || undefined,
-        'jest/globals': semver.satisfies(process.version, '>=14') || undefined,
+        'jest/globals': true,
       },
       rules: {
         // Allow `require` as it's needed for when we are using `jest.doMock` and friends
