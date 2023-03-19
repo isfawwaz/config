8313d8795ae663e925d569bac9cdf6275f301074
Ahmad Faris <isfawwaz@gmail.com>
2023-03-19 10:41:46 +0700

    chore: update commitlint config which disable body max line length

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,8 @@
 'use strict';
 
 module.exports = {
-  extends: [
-    '@commitlint/config-conventional',
-  ],
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-max-line-length': [0, 'always', 100],
+  },
 };
